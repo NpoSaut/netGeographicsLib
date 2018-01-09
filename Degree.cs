fe483281ea2812119b1faf69dacf071f55eeddef
Evgeniy Plyusnin <eninbox@gmail.com>
2018-01-09 17:49:10 +0500

    У Degree добавились члены для конвертации в Int 1e6 + выравнивание

--- Degree.cs
+++ Degree.cs
@@ -6,7 +6,8 @@ namespace Geographics
     public struct Degree : IComparable<Degree>
     {
         public Degree(double value) { Value = value; }
-        public Degree(int Degrees, int Minutes, double Seconds) : this(Degrees + (Minutes + Seconds / 60.0) / 60.0) { }
+        public Degree(int    Degrees, double Minutes) : this(Degrees + Minutes / 60.0) { }
+        public Degree(int    Degrees, int    Minutes, double Seconds) : this(Degrees + (Minutes + Seconds / 60.0) / 60.0) { }
 
         /// <summary>Значение величины угла в градусах</summary>
         public double Value { get; }
@@ -18,15 +19,19 @@ namespace Geographics
         public static implicit operator double(Degree v) { return v.Value; }
         public static implicit operator Degree(double v) { return new Degree(v); }
 
-        public Radian ToRadian() { return new Radian(Value * Math.PI / 180.0); }
-        public static implicit operator Radian(Degree v) { return v.ToRadian(); }
+        public                          Radian ToRadian() { return new Radian(Value * Math.PI / 180.0); }
+        public static implicit operator Radian(Degree v)  { return v.ToRadian(); }
 
-        public static Degree operator +(Degree v1, Degree v2) { return new Degree(v1.Value + v2.Value); }
-        public static Degree operator -(Degree v1, Degree v2) { return new Degree(v1.Value - v2.Value); }
-        public static Degree operator *(double m, Degree val) { return new Degree(m * val.Value); }
-        public static Degree operator *(Degree val, double m) { return new Degree(m * val.Value); }
-        public static Degree operator /(Degree val, double m) { return new Degree(val.Value / m); }
+        public static Degree operator +(Degree v1, Degree  v2)  { return new Degree(v1.Value + v2.Value); }
+        public static Degree operator -(Degree v1, Degree  v2)  { return new Degree(v1.Value - v2.Value); }
+        public static Degree operator *(double m, Degree   val) { return new Degree(m * val.Value); }
+        public static Degree operator *(Degree val, double m)   { return new Degree(m * val.Value); }
+        public static Degree operator /(Degree val, double m)   { return new Degree(val.Value / m); }
 
-        public override string ToString() { return string.Format("{0:F4}°", Value); }
+        public override string ToString() { return $"{Value:F4}°"; }
+
+        public int ToE6Int() { return (int)(Value * 1e6); }
+
+        public static Degree FromE6Int(int Value) { return new Degree(Value * 1e-6); }
     }
 }
